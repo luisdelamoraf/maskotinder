3a6bc65f35e499f29789c700b49c73902fe6c28d
raltu <si715235@gmail.com>
2020-04-25 20:51:32 -0500

    FormReg

--- inicio.js
+++ inicio.js
@@ -1,13 +1,18 @@
-let forma = document.querySelector("#reg");
+console.log("hola mundo");
+let FormReg = document.querySelector("#ModReg");
+console.log(FormReg);
 
 
-forma.addEventListener("change",()=>{
+FormReg.addEventListener("onclick",()=>{
+    console.log("inicio");
     let inputForma = document.querySelectorAll(":invalid");
     if(inputForma.length==0){
         if(document.querySelector("#RegPass1").value == document.querySelector("#RegPass2").value){
+            console.log("jaló");
             document.querySelector("#btnRegistro").removeAttribute("disabled");
         }else{
             document.querySelector("#btnRegistro").setAttribute("disabled","");
+            console.log("no jaló");
         }
     }
 });

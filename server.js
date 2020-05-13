9fe25bf2d15a7aac77bccaf978852923540a021e
luisdelamoraf <luise_delamora@hotmail.com>
2020-05-12 22:46:54 -0500

    make it better

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const randomize = require("randomatic");
 const app = express()
-const port = 3000;
+const port =  process.env.PORT || 3000;
 const mongoose = require('./db/mongodb-connect');
 const usuariosRouter = require("./routes/usuarios");
 const mascotasRouter = require("./routes/mascotas");
